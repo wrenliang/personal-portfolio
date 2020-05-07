a6cda0febb0585e76573e3da403c5e08a0e08a74
Wren Liang <wren.liang@gmail.com>
2020-05-07 17:37:44 -0600

    Updates

---
+++ precache-manifest.9b52d06f62225e52b7b72a3b3b878ac7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb43a45e508b17cd70bd199dabdfe9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f52824fdca8965caa4ec",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "dad833ec988e62b7f780",
+    "url": "/static/css/main.47220d6b.chunk.css"
+  },
+  {
+    "revision": "f52824fdca8965caa4ec",
+    "url": "/static/js/2.b5ce7810.chunk.js"
+  },
+  {
+    "revision": "50b5b45696acec9ddb130197e97912f5",
+    "url": "/static/js/2.b5ce7810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dad833ec988e62b7f780",
+    "url": "/static/js/main.1cea85ea.chunk.js"
+  },
+  {
+    "revision": "a5e68501379803401361",
+    "url": "/static/js/runtime-main.44b4814e.js"
+  },
+  {
+    "revision": "e33117eb554ff6531309abbfa408ee35",
+    "url": "/static/media/angle-double-left-solid.e33117eb.svg"
+  },
+  {
+    "revision": "14dc4eba1d5528ca797549dcc65f455a",
+    "url": "/static/media/angle-double-right-solid.14dc4eba.svg"
+  },
+  {
+    "revision": "4503d66a563e3edb7c618a21176cd8ae",
+    "url": "/static/media/jarvis-robot.4503d66a.png"
+  },
+  {
+    "revision": "929e19b43e6044c66db90279d9f4b1fe",
+    "url": "/static/media/mountietocat.929e19b4.png"
+  },
+  {
+    "revision": "2cff85eb984dc3b4908b22a47bfe5eba",
+    "url": "/static/media/profile-pic-compressed.2cff85eb.png"
+  },
+  {
+    "revision": "cf6924f3c41c80085e6207054141968e",
+    "url": "/static/media/projects-keepfresh.cf6924f3.png"
+  },
+  {
+    "revision": "4f904577e0023cb6527b0c5faadb72c8",
+    "url": "/static/media/projects-personal-website.4f904577.png"
+  },
+  {
+    "revision": "ddffece84cfb2b0328bcb82ca19be7b6",
+    "url": "/static/media/projects-sprouts.ddffece8.png"
+  },
+  {
+    "revision": "234e45954f1f9c8bc0404e1b05caa605",
+    "url": "/static/media/rawson-frame.234e4595.jpg"
+  },
+  {
+    "revision": "c20e114276fd4143467a2c8ed1d4d718",
+    "url": "/static/media/website-resume.c20e1142.pdf"
+  }
+]);
